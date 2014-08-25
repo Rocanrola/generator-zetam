926b2e4752147e5f7c5da76ec50514c53d50a9ad
Marcelo Zapaia <mzapaia@Admins-MacBook-Pro.local>
2014-08-25 17:31:01 -0300

    Commit message

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp');
 var z = require('zetam');
 
-z.gulp(gulp);
+var config = {
+	paths:[__dirname+'/globals']
+}
+
+z.gulp(gulp,config);
 
 gulp.task('default', ['zetam']);
